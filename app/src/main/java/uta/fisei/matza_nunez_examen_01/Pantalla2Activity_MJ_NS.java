166071047372c43bbc2a6841f11ba8203b1a3ad6
TOSHIBA <nlshirley08@gmail.com>
2022-12-08 18:32:32 -0500

    MATZA NUNEZ 18:32

--- app/src/main/java/uta/fisei/matza_nunez_examen_01/Pantalla2Activity_MJ_NS.java
+++ app/src/main/java/uta/fisei/matza_nunez_examen_01/Pantalla2Activity_MJ_NS.java
@@ -2,13 +2,64 @@ package uta.fisei.matza_nunez_examen_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pantalla2Activity_MJ_NS extends AppCompatActivity {
+
+
+    EditText editTextnumero ;
+    int contador =0;
+    List<String> lstDatos = new ArrayList<String>();
+    ListView listViewDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2_mj_ns);
+
+
+        editTextnumero = findViewById(R.id.editTextNumero);
+        listViewDatos = findViewById(R.id.listViewDatos);
+    }
+
+
+    public void  ingresar (View view){
+
+        String numero = editTextnumero.getText().toString();
+        lstDatos.add(numero);
+        contador++;
+
+        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,lstDatos);
+        listViewDatos.setAdapter(adapter);
+        editTextnumero.setText("");
+    }
+
+
+
+    public void cerrar (View view) {
+
+
+        String[] lstDatosSalida = new String[contador];
+
+        int i=0;
+        while (i<contador)
+        {
+            lstDatosSalida[i]=lstDatos.get(i);
+            i++ ;
+        }
+        Intent intent = new Intent();
+        intent.putExtra("lstDatos", lstDatosSalida);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 }

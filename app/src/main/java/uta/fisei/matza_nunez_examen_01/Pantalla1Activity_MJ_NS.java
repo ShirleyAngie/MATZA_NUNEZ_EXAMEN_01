adb1409bea6588a8dcc56a2ad8ed55faef4fba59
TOSHIBA <nlshirley08@gmail.com>
2022-12-08 18:16:37 -0500

    MATZA NUNEZ 18:15

--- app/src/main/java/uta/fisei/matza_nunez_examen_01/Pantalla1Activity_MJ_NS.java
+++ app/src/main/java/uta/fisei/matza_nunez_examen_01/Pantalla1Activity_MJ_NS.java
@@ -1,14 +1,55 @@
 package uta.fisei.matza_nunez_examen_01;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pantalla1Activity_MJ_NS extends AppCompatActivity {
+
+
+    String[] lstDatosRecibidos;
+
+
+
+    ActivityResultLauncher<Intent> activityResult =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+
+                    if (result.getResultCode() == Activity.RESULT_OK)
+                    {
+
+                        Intent data = result.getData();
+
+                        Bundle datos = result.getData().getExtras();
+                        lstDatosRecibidos = datos.getStringArray("lstDatos");
+
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla1_mj_ns);
+    }
+
+
+
+    public void segundoActivity (View view) {
+        Intent intent = new Intent(this,Pantalla2Activity_MJ_NS.class);
+        activityResult.launch(intent);
     }
 }

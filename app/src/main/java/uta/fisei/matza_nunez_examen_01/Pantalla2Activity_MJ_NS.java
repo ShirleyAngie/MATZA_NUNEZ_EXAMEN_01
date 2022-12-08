dd7ff440e5fbcfb8a0b085e58b2c8886fd8b87fd
TOSHIBA <nlshirley08@gmail.com>
2022-12-08 18:03:06 -0500

    MATZA NUNEZ 18:00

---
+++ app/src/main/java/uta/fisei/matza_nunez_examen_01/Pantalla2Activity_MJ_NS.java
@@ -0,0 +1,14 @@
+package uta.fisei.matza_nunez_examen_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla2Activity_MJ_NS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2_mj_ns);
+    }
+}
